94ad0aad83dffc93214809ab509ba754bf09e98e
victorpereiira <victorrpsantos@gmail.com>
2023-01-31 01:13:29 -0300

    fix: two words

--- main.js
+++ main.js
@@ -41,26 +41,26 @@ const data = [
         ],
         "audio_init": "8"
     },
-    {
-        "id": "$dialogue-17",
-        "title": "Listen and complete this dialogue.",
-        "phases": [
-            "[P = Paul; B = Brinitha ]",
-            "P: Hi, Brinitha.",
-            "B: Hi, Paul.",
-            "P: How's it $going ?",
-            "B: Fine, fine.",
-            "P: What $are you $doing at the moment?",
-            "B: Oh, I ['m installing] Nero.",
-            "P: How are you getting on?",
-            "B: Well, I ['m setting] up a network. I['m using] Microsoft Server.",
-            "P: Right. Where is Jackie today? Do you know?",
-            "B: Yes. She is on a training course today. She's $learning about the new database system.",
-            "P: What about Mary and Imran? Where are they?",
-            "B: They [aren't coming] in today. They have a day off. "
-        ],
-        "audio_init": "7"
-    },
+    // {
+    //     "id": "$dialogue-17",
+    //     "title": "Listen and complete this dialogue.",
+    //     "phases": [
+    //         "[P = Paul; B = Brinitha ]",
+    //         "P: Hi, Brinitha.",
+    //         "B: Hi, Paul.",
+    //         "P: How's it $going ?",
+    //         "B: Fine, fine.",
+    //         "P: What $are you $doing at the moment?",
+    //         "B: Oh, I ['m installing] Nero.",
+    //         "P: How are you getting on?",
+    //         "B: Well, I ['m setting] up a network. I'm using Microsoft Server.",
+    //         "P: Right. Where is Jackie today? Do you know?",
+    //         "B: Yes. She is on a training course today. She's $learning about the new database system.",
+    //         "P: What about Mary and Imran? Where are they?",
+    //         "B: They [aren't coming] in today. They have a day off. "
+    //     ],
+    //     "audio_init": "7"
+    // },
     {
         "id": "$dialogue-20",
         "title": "Listen to Sarah and George. Complete this dialogue.",
@@ -125,7 +125,6 @@ function generate_dialogue(dialogue) {
         const dialogue = el.id.slice(1)
         menu_elements += `<button id="${el.id.split("-")[1]}">${dialogue[0].toUpperCase() + dialogue.slice(1)}</button>`
     })
-
 
     $("#app").innerHTML = /*html*/`
         <div class="menu">${menu_elements}</div>
@@ -252,8 +251,11 @@ function generate_dialogue(dialogue) {
         trust_array = trust_array.map(word => word.replace(/[\[\]$]/g, ""))
 
         let temp = ""
+        let wordTemp = ""
         let isEmpty = false
         let count = 0
+        let boxTemp = []
+        let allCorrect = true
 
         $$(".container input").forEach((el, idx) => {
             let text = el.parentNode.innerHTML
@@ -265,21 +267,22 @@ function generate_dialogue(dialogue) {
             }
 
             let inputCount = (text.match(/<input/g) || []).length;
-            if (inputCount < 2) {
-                $(clx).innerHTML = text.replace(Elinput(idx, el.className.split("-")[1]), `<span class= "word-${idx}"> ${el.value}</span> `)
-
+            if (inputCount === 1) {
+                $(clx).innerHTML = text.replace(Elinput(idx, el.className.split("-")[1]), `<span class= "word-${idx}"> ${el.value}</span>`)
                 const word = $(`.word-${idx}`)
                 word.style.fontWeight = "700"
-
                 if (trust_array[idx] === el.value) {
                     word.style.color = "#2ee141"
                 } else {
                     word.style.color = "#ff2020"
+                    allCorrect = false
                 }
             } else {
                 if (count === 0) {
                     temp = text.replace(Elinput(idx, el.className.split("-")[1]), `<span class= "word-${idx}"> ${el.value}</span>`)
                     count++
+
+                    boxTemp.push({ ta: trust_array[idx], elValue: el.value, idxEl: idx })
                 } else {
                     const clx = "#phase-" + (el.className.split("-")[1]);
                     $(clx).innerHTML =
@@ -287,15 +290,26 @@ function generate_dialogue(dialogue) {
                     count = 0
                     temp = ""
 
-                    for (let i = idx; i < (idx + inputCount); i++) {
-                        const word = $(`.word-${i - 1}`)
-                        word.style.fontWeight = "700"
-
-
-                        if (trust_array[idx] === el.value) {
-                            word.style.color = "#2ee141"
+                    for (let i = 0; i < 2; i++) {
+                        if (i === 0) {
+                            const { idxEl, ta, elValue } = boxTemp[0]
+                            const word = $(`.word-${idxEl}`)
+                            word.style.fontWeight = "700"
+                            if (ta === elValue) {
+                                word.style.color = "#2ee141"
+                            } else {
+                                word.style.color = "#ff2020"
+                                allCorrect = false
+                            }
                         } else {
-                            word.style.color = "#ff2020"
+                            const word = $(`.word-${idx}`)
+                            word.style.fontWeight = "700"
+                            if (trust_array[idx] === el.value) {
+                                word.style.color = "#2ee141"
+                            } else {
+                                word.style.color = "#ff2020"
+                                allCorrect = false
+                            }
                         }
                     }
                 }
@@ -320,13 +334,27 @@ function generate_dialogue(dialogue) {
                 $containerTip.innerHTML = ""
                 $containerTip.style.border = "2px solid #c4c4c4"
                 $containerTip.style.display = "flex"
+                $containerTip.style.flexWrap = "wrap"
+                $containerTip.style.flexBasis = "50%"
                 $containerTip.style.justifyContent = "space-between"
+                let tawidx = 5
+                let ta_temp = ""
                 trust_array
                     .sort(() => Math.random() - 0.5)
-                    .forEach(ta_word => {
+                    .forEach((ta_word, ta_idx) => {
                         $containerTip.innerHTML += `<p>${ta_word}</p>`
+                        // .forEach((ta_word, ta_idx) => {
+                        //     if (ta_idx < tawidx) {
+                        //         ta_temp += `<p>${ta_word}</p>`
+                        //     } else {
+                        //         $containerTip.innerHTML += `<div>${ta_temp}</div>`
+                        //         ta_temp = ""
+                        //         tawidx += 5
+                        //     }
                     })
             })
+
+            allCorrect ? $tipBtn.click() : true
         }
 
         $sendBtn.addEventListener("click", () => {
