a3a1f24d011e14059ac6b05059ca3dbd4677565f
victorpereiira <victorrpsantos@gmail.com>
2023-02-07 20:21:22 -0300

    feat: add menu and action sections

--- main.js
+++ main.js
@@ -4,5 +4,4 @@ import App from "./src/App";
 (async () => {
     const dialogue_id = db[0].id.split("-")[1]
     document.body.insertAdjacentHTML('afterbegin', await App(db))
-    // $(`.menu button`).style.background = "#22de8c"
 })();
